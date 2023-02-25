29f522e4f094ca57363616d1d967fe24658f6a6a
YESENIA2012 <gyesenia173@gmail.com>
2023-02-25 15:55:23 -0500

    create microserver

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-
-// Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  const url = req.body.url;
+
+  if (!url.includes("https://") && !url.includes("http://")) {
+    return res.json({ error: "invalid url" });
+  } else {
+    next();
+  }
+});
+
+const arrayUrls = [];
+const counterUrl = [];
+
+app.post("/api/shorturl", (req, res) => {
+  const url = req.body.url;
+
+  if (url === "http://localhost:3000/api/shorturl/</short_url>") {
+    return res.redirect("/");
+  }
+
+  res.json({ original_url: url, short_url: 1 });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
