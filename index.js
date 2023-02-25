25ef97e4e0e86d80c8cb4fe98f0cc53165626831
YESENIA2012 <gyesenia173@gmail.com>
2023-02-25 17:58:51 -0500

    some changes were made

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+const arrayUrls = [];
+const shortUrl = [];
 
 app.use(cors());
 app.use("/public", express.static(`${process.cwd()}/public`));
@@ -17,27 +19,27 @@ app.get("/api/hello", function (req, res) {
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
+app.get("/api/shorturl/:short_url", (req, res) => {
+  const numberUrlShort = parseInt(req.params.short_url);
+
+  if (shortUrl.includes(numberUrlShort)) {
+    return res.redirect(arrayUrls[numberUrlShort]);
+  }
+
+  res.json({ error: "Does not exist" });
+});
+
+app.post("/api/shorturl", (req, res, next) => {
   const url = req.body.url;
 
   if (!url.includes("https://") && !url.includes("http://")) {
     return res.json({ error: "invalid url" });
   } else {
+    arrayUrls.push(url);
+    shortUrl.push(arrayUrls.length);
+    res.json({ original_url: url, short_url: shortUrl.length - 1 });
     next();
   }
-});
-
-const arrayUrls = [];
-const counterUrl = [];
-
-app.post("/api/shorturl", (req, res) => {
-  const url = req.body.url;
-
-  if (url === "http://localhost:3000/api/shorturl/</short_url>") {
-    return res.redirect("/");
-  }
-
-  res.json({ original_url: url, short_url: 1 });
 });
 
 app.listen(port, function () {
